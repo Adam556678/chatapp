0f8e4f66883e33e071daee3c445c1cf79f9e83d1
Adam556678 <mahmudramadan75@yahoo.com>
2025-10-20 20:05:15 +0300

    add sending messages funtionality

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const socket = io();
+
+// Message from server
+socket.on('message', (message) => {
+    console.log(message);
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// On message submit
+chatForm.addEventListener('submit', (event) => {
+    // prevents submisssion to a file
+    event.preventDefault();
+
+    const msg = event.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+    // Clear input
+    event.target.elements.msg.value='';
+    event.target.elements.msg.focus();
+
+}); 
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">${message.text}</p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 const io = socketio(server);
 
 // Run when cline connect (Listen on connection event)
 io.on('connection', (socket) => {
     console.log("New WebSocket connection...");
+
+    socket.emit('message', formatMessage("ChatApp Bot", "Welcome to ChatApp"));
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', formatMessage("ChatApp Bot", "A user has joined the chat"));
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage("ChatApp Bot", "A user has left the chat"));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage("User", msg));
+    });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
